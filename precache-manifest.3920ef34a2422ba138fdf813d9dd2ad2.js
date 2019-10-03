4964169ad60695d7796410e80e13b0cd656ea8ad
DeniseSurekha <denisesurekha@gmail.com>
2019-10-03 15:15:21 +0530

    Updates

---
+++ precache-manifest.3920ef34a2422ba138fdf813d9dd2ad2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f0a0386b8762c10c2dc35741703d18c",
+    "url": "/reactjs-news-website/index.html"
+  },
+  {
+    "revision": "bfdd5465af26864d6dd4",
+    "url": "/reactjs-news-website/static/css/2.05409562.chunk.css"
+  },
+  {
+    "revision": "3528d9f2460bbaf48a51",
+    "url": "/reactjs-news-website/static/css/main.8c32f361.chunk.css"
+  },
+  {
+    "revision": "bfdd5465af26864d6dd4",
+    "url": "/reactjs-news-website/static/js/2.06d03dd1.chunk.js"
+  },
+  {
+    "revision": "3528d9f2460bbaf48a51",
+    "url": "/reactjs-news-website/static/js/main.f81e8228.chunk.js"
+  },
+  {
+    "revision": "d61aeec6b6a943f248d9",
+    "url": "/reactjs-news-website/static/js/runtime~main.cbfd4bca.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/reactjs-news-website/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/reactjs-news-website/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/reactjs-news-website/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/reactjs-news-website/static/media/slick.f97e3bbf.svg"
+  }
+]);
